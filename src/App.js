ea70f65ade2fb91dcdd4ddaaa3ad7310248f4ae5
scrumpygeo <scrumpygeo@yahoo.ca>
2021-04-09 15:24:30 +0100

    add json file

--- src/App.js
+++ src/App.js
@@ -1,5 +1,15 @@
+import { useState } from 'react'
+
+import Loading from './components/Loading'
+import Cars from './components/Cars'
 function App() {
-  return <div className='container'>App</div>
+  const [loading, setLoading] = useState(true)
+  const [cars, setCars] = useState([])
+  return (
+    <div className='container'>
+      <Cars />
+    </div>
+  )
 }
 
 export default App
