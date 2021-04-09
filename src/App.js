fac35cad1506754936bd4da7994a01cc6cd56df4
scrumpygeo <scrumpygeo@yahoo.ca>
2021-04-09 13:58:58 +0100

    add styling and images

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 function App() {
-  return <div>App</div>
+  return <div className='container'>App</div>
 }
 
 export default App
---
+++ src/components/Cars.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import Car from './Car'
+
+const Cars = () => {
+  return (
+    <div>
+      <Car />
+    </div>
+  )
+}
+
+export default Cars
