eb2ed53a4cbf1502176ed35a35eb7b9e80acaf13
scrumpygeo <scrumpygeo@yahoo.ca>
2021-04-09 15:37:33 +0100

    update loading component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ import Cars from './components/Cars'
 function App() {
   const [loading, setLoading] = useState(true)
   const [cars, setCars] = useState([])
+
+  if (loading) {
+    return (
+      <main>
+        <Loading />
+      </main>
+    )
+  }
   return (
     <div className='container'>
       <Cars />
